c9564a836fba29317cd7a2085833335f3f3b6f16
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-11-01 20:07:19 +0000

    Fixed logout button.

--- src/components/logoutButton.tsx
+++ src/components/logoutButton.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import { CiLogout } from "react-icons/ci";
+import { IoIosLogOut } from "react-icons/io";
 
 export default function LogoutButton() {
     return (
-        <Link href="/auth/logout" className="absolute top-5 left-5">
-            <CiLogout className="w-6 h-6 hover:scale-110 duration-100 transform" />
+        <Link href="/auth/logout" className="absolute top-5 right-5">
+            <IoIosLogOut className="w-6 h-6 hover:scale-110 duration-100 transform" />
         </Link>
     )
 }
