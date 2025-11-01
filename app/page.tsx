dba0e1448ac8dbc3ff107d207d49e3bfad6e20c3
mrmounment <122111436+mrmounment@users.noreply.github.com>
2025-11-01 14:22:51 +0000

    Added Chatbot
    Added the talkjs chatbot, very basic functionality currently

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 "use client"
+import Chat from './dashboard/talkjs';
 
 export default function Home() {
   return (
-
+      <div>
+        <h1>Hello</h1>
+          <Chat />
+      </div>
   );
 }
