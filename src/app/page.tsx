4877bfa1df6052101b3cf1bc201061656613e8b1
mrmounment <122111436+mrmounment@users.noreply.github.com>
2025-11-01 15:15:21 +0000

    Added a tailwind box
    tailwind boxes for styling and adding components inside

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 "use client"
-import Chat from './dashboard/talkjs';
+import Chat from '../components/talkjs';
+import Box from '../components/box';
+
 
 export default function Home() {
   return (
       <div>
         <h1>Hello</h1>
+
+          <Box classname="max-w-md mx-auto mt-8">
+              <h2 className="text-xl font-bold mb-2">Welcome</h2>
+              <p>This is inside a styled Tailwind box.</p>
           <Chat />
+          </Box>
       </div>
   );
 }
