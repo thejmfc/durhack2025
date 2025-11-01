fdd21f1260f32c945c242e3e41173b2ebe0b720c
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-11-01 13:17:16 +0000

    Removed example.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
 "use client"
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const greetings = ["Hello", "Hi", "Heya", "EUAN IM WATCHING YOU"]
-  const [greeting, setGreeting] = useState<string | null>(null);
-
-
-  useEffect(() => {
-    setGreeting(greetings[Math.floor(Math.random() * greetings.length)])
-  }, [])
-
-
   return (
-   <h1>{greeting}</h1>
+
   );
 }
