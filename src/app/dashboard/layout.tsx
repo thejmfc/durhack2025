5c3fe934d35ead68ea9a3afec638a261af147421
Oliver GD <114223604+OGD311@users.noreply.github.com>
2025-11-01 16:58:39 +0000

    Require auth to access dashboard and related routes.

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import RequireAuth from "@/context/RequireAuth";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <RequireAuth>
+        {children}
+    </RequireAuth>
+  );
+}
