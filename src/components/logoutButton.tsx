3159185ac72baf634a324a618b5d28f976323cc5
mrmounment <122111436+mrmounment@users.noreply.github.com>
2025-11-02 10:43:40 +0000

    Logout button changed
    changed logout button to white

--- src/components/logoutButton.tsx
+++ src/components/logoutButton.tsx
@@ -3,7 +3,7 @@ import { IoIosLogOut } from "react-icons/io";
 
 export default function LogoutButton() {
     return (
-        <Link href="/auth/logout" className="absolute top-5 right-5">
+        <Link href="/auth/logout" className="absolute top-5 right-5 bg-white rounded-full p-2">
             <IoIosLogOut className="w-6 h-6 hover:scale-110 duration-100 transform" />
         </Link>
     )
