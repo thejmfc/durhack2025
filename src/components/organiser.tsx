c93aa8e86e5ebd44ea2a7dbcd70d61baf4a1edd9
mrmounment <122111436+mrmounment@users.noreply.github.com>
2025-11-02 10:42:19 +0000

    More styles
    more styles

--- src/components/organiser.tsx
+++ src/components/organiser.tsx
@@ -12,7 +12,7 @@ export default function OrganiserCard({
           <h1 className="text-xl font-bold text-gray-900">
             {first_name} {last_name}
           </h1>
-          <span className="bg-gradient-to-r from-blue-500 to-indigo-500 text-white px-4 py-2 text-md font-semibold rounded-full shadow-md">
+          <span className="bg-slate-900 text-white px-4 py-2 text-md font-semibold rounded-full shadow-md">
             {role}
           </span>
         </section>
